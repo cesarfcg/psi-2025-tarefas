03875267bd0416d6e03a10336ac8fd799dc4c8c3
Fernando César <fernandocesar4141@gmail.com>
2025-08-01 16:05:27 -0300

    tarefa05

--- tarefa05/blog/models.py
+++ tarefa05/blog/models.py
@@ -6,10 +6,13 @@ class Postagem(models.Model):
     descricao = models.TextField()
     data = models.DateField(date.today)
     imagem = models.ImageField( upload_to="media/",null=True, blank=True)
-
+    def __str__(self):
+        return self.titulo
 class Header(models.Model):
     titulo = models.CharField(max_length=50)
     link = models.CharField(max_length=100)
-
+    def __str__(self):
+        return self.titulo
 class Footer(models.Model):
     descricao = models.TextField()
+    
--- tarefa05/blog/views.py
+++ tarefa05/blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render,get_object_or_404
-from . models import Postagem
+from . models import Postagem,Footer,Header
 # Create your views here.
 def index(request):
     postagem = Postagem.objects.all()
-    context = {"postagem":postagem}
+    footer = Footer.objects.first()
+    header = Header.objects.first()
+    context = {"postagem":postagem, "footer":footer, "header":header}
     return render(request,'blog/index.html',context)
 def noticia(request, id_post):
     postagem = get_object_or_404(Postagem, id=id_post)
-    context = {"postagem":postagem}
+    footer = Footer.objects.first()
+    header = Header.objects.first()
+    context = {"postagem":postagem, "footer":footer, "header":header}
     return render(request,'blog/noticia.html',context)
